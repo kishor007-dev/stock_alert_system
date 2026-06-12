9330e6bed46a9fcda5861e0b32daf5e4bf7602a8
kishor007-dev <kishorchar07@gmail.com>
2026-06-12 12:52:23 +0530

    removed firebase json

--- backend/server.js
+++ backend/server.js
@@ -60,9 +60,15 @@ app.use(express.json());
 connectDB();
 
 try {
-    const serviceAccount = require('./config/firebase-service-account.json');
-    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-    console.log('✅ Firebase Admin Initialized');
+    admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+});
+
+console.log('✅ Firebase Admin Initialized');
 } catch (error) {
     console.error('❌ Firebase Admin Error. Ensure config/firebase-service-account.json exists.');
     process.exit(1);
